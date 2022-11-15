f0216c3232d6f657ead4ba1b439d1774e564d1d2
NotAGameDev07 <lokeshspatel07@gmail.com>
2022-11-15 12:55:59 -0500

    made event changer thingy

--- src/render.js
+++ src/render.js
@@ -1,7 +1,19 @@
-const btn = document.getElementById("btn");
+const btnS = document.getElementById("btnS");
+const btnE = document.getElementById("btnE");
 const text = document.getElementById("text");
-btn.onlick = yes;
+
+btnS.onclick = yes;
+btnE.onclick = no;
+
+var globals = {};
+globals.t = () => {};
 
 function yes() {
-  document.getElementById("text").innerText = "no";
+  globals.t = setInterval(() => {
+    text.innerText = document.getElementById("tinput").value;
+  }, 1000);
+}
+
+function no() {
+  clearInterval(globals.t);
 }
