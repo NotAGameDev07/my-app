2d96421b11ef0df82eca38067467e50371baee48
NotAGameDev07 <lokeshspatel07@gmail.com>
2022-11-15 07:23:38 -0500

    modified web app

---
+++ src/render.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn");
+const text = document.getElementById("text");
+btn.onlick = yes;
+
+function yes() {
+  document.getElementById("text").innerText = "no";
+}
